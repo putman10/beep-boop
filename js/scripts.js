20a21b35de9297fd91818b3f263abb330fdeaf56
Ryan Putman <putman10@me.com>
2018-06-08 13:01:17 -0700

    add if and else statement to loop to check that number is greater than 0 and alert user if it isnt

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,16 @@
 
 function loopNumber(userNumber) {
 
-  var loopAmmount = userNumber;
-
   $("#result ul").text(" ");
 
-
- for (x = 0; x <= loopAmmount; x++) {
-  var loopCurrentNumber = x.toString();
-  checkNumber(loopCurrentNumber);
- }
+  if (userNumber > 0) {
+   for (x = 0; x <= userNumber; x++) {
+    var loopCurrentNumber = x.toString();
+    checkNumber(loopCurrentNumber);
+   }
+  } else if (true) {
+alert("Please enter a number greater than 0")
+  }
 
  $("#result").slideDown();
 
@@ -23,7 +24,7 @@ function checkNumber(userNumber){
   var divisibleByThree = parseInt(userNumber) % 3 === 0
 
   if (divisibleByThree === true) {
-    customName(userNumber);
+    customName();
   } else if (includesZero === true && includesOne === false) {
     $("#result ol").append("<li>" +  "Beep!" + "</li>");
   } else if (includesOne === true) {
@@ -33,13 +34,16 @@ function checkNumber(userNumber){
   }
 }
 
-function customName(userNumber) {
-  var userInput = $("#user-name").val();
+function customName() {
+  var userName = $("#user-name").val();
 
   $("#result ol").append("<li>" + "I'm sorry, " + "<span class='name'></span>" + ". I'm afraid I can't do that." + "</li>");
 
-  $("span.name").text(userInput);
-
+  if ($("#user-name").val()) {
+    $("span.name").text(userName);
+  } else {
+    $("span.name").text("Dave");
+  }
 }
 
 

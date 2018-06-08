43ba3c3f8642978c8132cc902c8050d64d111c98
Ryan Putman <putman10@me.com>
2018-06-08 14:30:27 -0700

    add function to reverse the loop and removed list styles adding usernumber instead. So that it works when reversed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 // BUSINESS LOGIC
 
 function loopNumber(userNumber) {
+  var order = "reverse";
 
   $("#result ul").text(" ");
 
-  if (userNumber >= 0) {
+  if (userNumber >= 0 && order === "notReverse") {
    for (x = 0; x <= userNumber; x++) {
     var loopCurrentNumber = x.toString();
     checkNumber(loopCurrentNumber);
    }
-  } else if (true) {
-alert("Please enter a number greater than 0")
+ } else if (userNumber >= 0 && order === "reverse") {
+   for (x = userNumber; x >= 0; x--) {
+    var loopCurrentNumber = x.toString();
+    checkNumber(loopCurrentNumber);
+   }
+  } else if (userNumber < 0) {
+ alert("Please enter a number greater than 0")
+  } else {
+    alert("this will work in a bit")
+
   }
 
  $("#result").slideDown();
@@ -24,20 +33,20 @@ function checkNumber(userNumber){
   var divisibleByThree = parseInt(userNumber) % 3 === 0
 
   if (divisibleByThree === true) {
-    customName();
+    customName(userNumber);
   } else if (includesZero === true && includesOne === false) {
-    $("#result ol").append("<li>" +  "Beep!" + "</li>");
+    $("#result ul").append("<li>" + userNumber + " = " + "Beep!" + "</li>");
   } else if (includesOne === true) {
-    $("#result ol").append("<li>" +  "Boop!" + "</li>");
+    $("#result ul").append("<li>" + userNumber + " = " + "Boop!" + "</li>");
   } else {
-    $("#result ol").append("<li>" + parseInt(userNumber) + "</li>");
+    $("#result ul").append("<li>" + userNumber + " = " + parseInt(userNumber) + "</li>");
   }
 }
 
-function customName() {
+function customName(listNumber) {
   var userName = $("#user-name").val();
 
-  $("#result ol").append("<li>" + "I'm sorry, " + "<span class='name'></span>" + ". I'm afraid I can't do that." + "</li>");
+  $("#result ul").append("<li>" + listNumber + " = " + "I'm sorry, " + "<span class='name'></span>" + ". I'm afraid I can't do that." + "</li>");
 
   if ($("#user-name").val()) {
     $("span.name").text(userName);
@@ -50,9 +59,8 @@ function showNameField() {
   var nameSelection = $("input:radio[name=customInputName]").val();
 
   if (nameSelection === "yes") {
-$(".form-name").slideToggle();
+  $(".form-name").slideToggle();
   } else {
-
   }
  }
 

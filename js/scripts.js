dd42ff5e2c1b5071b475bd1db53896f95ef9bdce
Ryan Putman <putman10@me.com>
2018-06-08 09:48:42 -0700

    add variable for checking array with .includes for 0s or 1s

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 // BUSINESS LOGIC
 
 function checkNumber(userNumber){
-  $("#result").text(userNumber)
+  var userInputSplit = userNumber.split("");
+  var includesOne = userInputSplit.includes("1");
+  var includesTwo = userInputSplit.includes("0");
+console.log(includesOne);
+  // if (includesOne === true) {
+  //   alert("includes 1");
+  // } else if (includesTwo === true) {
+  //   alert("includes 0");
+  // } else {
+  //   alert("includes neither");
+  // }
+
+  $("#result").text(userInputSplit);
   $("#result").slideDown();
+
+}
+
+function checkNumber3(userNumber1){
+
 }
 
 // USER INTERFACE LOGIC
@@ -13,7 +30,6 @@ $(function(){
     event.preventDefault();
     var userInput = $("#user-input").val();
     checkNumber(userInput);
-    console.log(userInput);
 
   });
 

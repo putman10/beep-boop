9b26bdc71f696d12fee9271d19588cffd4bcfebf
Ryan Putman <putman10@me.com>
2018-06-08 09:14:32 -0700

    add initial script including the function to collect the user input push to a function and display the number in #result unconverted.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 // BUSINESS LOGIC
 
 function checkNumber(userNumber){
+  $("#result").text(userNumber)
+  $("#result").slideDown();
 }
 
 // USER INTERFACE LOGIC
+
+$(function(){
+
+  $("#beep-boop-form").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("#user-input").val();
+    checkNumber(userInput);
+    console.log(userInput);
+
+  });
+
+
+});

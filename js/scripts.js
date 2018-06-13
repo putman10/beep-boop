a3ece3024aedba87654c47cea14386861d8ccdb0
Ryan Putman <putman10@me.com>
2018-06-12 23:49:02 -0700

    refactor code and rename variables and returns to match what they represent

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,26 @@
 
 function loopNumber(userNumber) {
   var order = $("#sortOrder").val();
-  var finalarray3 = [];
+  var finalArray = [];
 
   if (userNumber >= 0 && order === "1") {
    for (x = 0; x <= userNumber; x++) {
     var loopCurrentNumber = x.toString();
     var final = checkNumber(loopCurrentNumber);
-    finalarray3.push(x + " =" + final + "<br>");
-    
+    finalArray.push(x + " =" + final + "<br>");
    }
  } else if (userNumber >= 0 && order === "2") {
    for (x = userNumber; x >= 0; x--) {
     var loopCurrentNumber = x.toString();
     var final = checkNumber(loopCurrentNumber);
-    finalarray3.push(x + " =" + final + "<br>");
+    finalArray.push(x + " =" + final + "<br>");
    }
   } else if (userNumber < 0) {
-    finalarray3.push("Please enter a number greater than 0");
+    finalArray.push("Please enter a number greater than 0");
   } else {
-    finalarray3.push("Try Again");
+    finalArray.push("Try Again");
   }
-  return finalarray3;
+  return finalArray;
 }
 
 function checkNumber(userNumber){
@@ -33,60 +32,55 @@ function checkNumber(userNumber){
   var newArray = [];
 
   if (divisibleByThree === true) {
-    newArray.push(" I'm Sorry, Dave. I'm afraid I can't do that.")
-
+    newArray.push(" I'm Sorry, <span class='name'></span>. I'm afraid I can't do that.")
   } else if (includesZero === true && includesOne === false) {
     newArray.push(" Beep!")
   } else if (includesOne === true) {
     newArray.push(" Boop!")
-    } else {
+  } else {
     newArray.push(" " + userNumber)
   }
-
   return newArray;
 }
-
-// function customName(listNumber) {
-//   var userName = $("#user-name").val();
-//   var nameSelection = $("input:radio[name=customInputName]:checked").val();
-//
-//   $("#result ul").append("<li>" + listNumber + " = " + "I'm sorry, " + "<span class='name'></span>" + ". I'm afraid I can't do that." + "</li>");
-//
-//   if (nameSelection === "yes") {
-//     $("span.name").text(userName);
-//   } else if (nameSelection === "no"){
-//     $("span.name").text("Dave");
-//   } else {
-//
-//   }
-// }
-
-// function showNameField() {
-//   var nameSelection = $("input:radio[name=customInputName]:checked").val();
-//
-//   if (nameSelection === "yes") {
-//   $(".form-name").slideDown();
-//   } else {
-//     $(".form-name").slideUp();
-//   }
-//  }
 
 // USER INTERFACE LOGIC
 
 $(function(){
 
-  // $('input:radio[name=customInputName]').change(function(){
-  //   showNameField();
-  // });
+  $('input:radio[name=customInputName]').change(function(){
+    var userName = $("#user-name").val();
+    var nameSelection = $("input:radio[name=customInputName]:checked").val();
+
+    $(".form-name").slideToggle();
+
+    if (nameSelection === "yes" && userName === "") {
+     $("span.name").text("Dave");
+   } else if (nameSelection === "yes") {
+     $("span.name").text(userName);
+   } else {
+     $("span.name").text("Dave");
+   }
+   
+  });
 
   $("#beep-boop-form").submit(function(event) {
     event.preventDefault();
     var userInput = $("#user-input").val();
-    var finalFinal = loopNumber(userInput).join(" ");
-    $("#resultArea").html(finalFinal);
+    var finalResult = loopNumber(userInput).join(" ");
+    var userName = $("#user-name").val();
+    var nameSelection = $("input:radio[name=customInputName]:checked").val();
+
+    $("#resultArea").html(finalResult);
+
+    if (nameSelection === "yes" && userName === "") {
+      $("span.name").text("Dave");
+    } else if (nameSelection === "yes") {
+      $("span.name").text(userName);
+    } else {
+      $("span.name").text("Dave");
+    }
+
     $("#resultArea").slideDown();
-    debugger;
 
   });
-
 });

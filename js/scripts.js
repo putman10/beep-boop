11024f651a0a9932eac36186555b91fc26b30724
Ryan Putman <putman10@me.com>
2018-06-12 22:35:07 -0700

    refactor code even more to seperate logic.

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,27 @@
 
 function loopNumber(userNumber) {
   var order = $("#sortOrder").val();
-
-  $("#result ul").text(" ");
+  var finalarray3 = [];
 
   if (userNumber >= 0 && order === "1") {
    for (x = 0; x <= userNumber; x++) {
     var loopCurrentNumber = x.toString();
-    checkNumber(loopCurrentNumber);
+    var final = checkNumber(loopCurrentNumber);
+    finalarray3.push(final);
+  
    }
  } else if (userNumber >= 0 && order === "2") {
    for (x = userNumber; x >= 0; x--) {
     var loopCurrentNumber = x.toString();
-    checkNumber(loopCurrentNumber);
+    var final = checkNumber(loopCurrentNumber);
+    finalarray3.push(final);
    }
   } else if (userNumber < 0) {
  alert("Please enter a number greater than 0")
   } else {
     alert("this will work in a bit")
-
   }
-
- $("#resultArea").slideDown();
-
+  return finalarray3;
 }
 
 function checkNumber(userNumber){
@@ -31,55 +30,63 @@ function checkNumber(userNumber){
   var includesOne = userInputSplit.includes("1");
   var includesZero = userInputSplit.includes("0");
   var divisibleByThree = parseInt(userNumber) % 3 === 0
+  var newArray = [];
 
   if (divisibleByThree === true) {
-    customName(userNumber);
+    newArray.push(" I'm Sorry, Dave. I'm afraid I can't do that.")
+
   } else if (includesZero === true && includesOne === false) {
-    $("#result ul").append("<li>" + userNumber + " = " + "Beep!" + "</li>");
+    newArray.push(" Beep!")
   } else if (includesOne === true) {
-    $("#result ul").append("<li>" + userNumber + " = " + "Boop!" + "</li>");
-  } else {
-    $("#result ul").append("<li>" + userNumber + " = " + parseInt(userNumber) + "</li>");
+    newArray.push(" Boop!")
+    } else {
+    newArray.push(" " + userNumber)
   }
+
+  return newArray;
 }
 
-function customName(listNumber) {
-  var userName = $("#user-name").val();
-  var nameSelection = $("input:radio[name=customInputName]:checked").val();
+// function customName(listNumber) {
+//   var userName = $("#user-name").val();
+//   var nameSelection = $("input:radio[name=customInputName]:checked").val();
+//
+//   $("#result ul").append("<li>" + listNumber + " = " + "I'm sorry, " + "<span class='name'></span>" + ". I'm afraid I can't do that." + "</li>");
+//
+//   if (nameSelection === "yes") {
+//     $("span.name").text(userName);
+//   } else if (nameSelection === "no"){
+//     $("span.name").text("Dave");
+//   } else {
+//
+//   }
+// }
 
-  $("#result ul").append("<li>" + listNumber + " = " + "I'm sorry, " + "<span class='name'></span>" + ". I'm afraid I can't do that." + "</li>");
-
-  if (nameSelection === "yes") {
-    $("span.name").text(userName);
-  } else if (nameSelection === "no"){
-    $("span.name").text("Dave");
-  } else {
-
-  }
-}
-
-function showNameField() {
-  var nameSelection = $("input:radio[name=customInputName]:checked").val();
-
-  if (nameSelection === "yes") {
-  $(".form-name").slideDown();
-  } else {
-    $(".form-name").slideUp();
-  }
- }
+// function showNameField() {
+//   var nameSelection = $("input:radio[name=customInputName]:checked").val();
+//
+//   if (nameSelection === "yes") {
+//   $(".form-name").slideDown();
+//   } else {
+//     $(".form-name").slideUp();
+//   }
+//  }
 
 // USER INTERFACE LOGIC
 
 $(function(){
 
-  $('input:radio[name=customInputName]').change(function(){
-    showNameField();
-  });
+  // $('input:radio[name=customInputName]').change(function(){
+  //   showNameField();
+  // });
 
   $("#beep-boop-form").submit(function(event) {
     event.preventDefault();
     var userInput = $("#user-input").val();
-    loopNumber(userInput);
+    var finalFinal = loopNumber(userInput);
+    $("#resultArea").text(finalFinal);
+    $("#resultArea").slideDown();
+    debugger;
+
   });
 
 });
